f8add88e2ef5111e744b174ceb99c4ad1c1bc793
quinhotheone <zaquemnevess@gmail.com>
2025-09-16 22:17:33 -0300

    Prática 2 de Programação 2

---
+++ ItemVenda.java
@@ -0,0 +1,31 @@
+public class ItemVenda {
+
+    private Produto produtoVendido;
+    private int quantidade;
+
+    public ItemVenda(Produto produtoVendido, int quantidade) {
+        this.produtoVendido = produtoVendido;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProdutoVendido() {
+        return produtoVendido;
+    }
+
+    public void setProdutoVendido(Produto produtoVendido) {
+        this.produtoVendido = produtoVendido;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularTotal() {
+        return getQuantidade() * getProdutoVendido().getPreco();
+    }
+
+}
